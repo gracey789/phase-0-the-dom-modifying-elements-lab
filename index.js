adef8c4e99e9fa00c1b780133bff92566cfd82c4
Gracey789 <Gmwende487@gmail.com>
2024-03-30 18:03:12 +0300

    feat: modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+/// Define a constant variable
+const testVar = "hi";
+
+// Remove the main element
+document.querySelector("main#main").remove();
+
+// Create a new header element
+const newHeader = document.createElement("h1");
+
+// Assign an id to the new header
+newHeader.id = "victory";
+
+// Set the inner HTML content of the new header
+newHeader.innerHTML = "Flatiron School is the champion!";
+
+// Assign a class name to the new header
+newHeader.className = "victory";
